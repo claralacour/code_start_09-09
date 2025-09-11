5ee8e0e50045e300cd8b88d41482a2ac59a468fb
Clara la Cour <denlacour@gmail.com>
2025-09-11 12:08:59 +0200

    mere

---
+++ 5,5.js
@@ -0,0 +1,35 @@
+//opg 1
+for (let i = 1; i < 6; i++) {
+    console.log(i)
+};
+
+//opg 2
+for (let i = 2; i < 11; i++) {
+    if (i % 2 == 0)
+        console.log(i)
+};
+
+//opg 3
+for (let i = 65; i <= 70; i++) {
+    console.log(String.fromCharCode(i))
+};
+
+//opg 4
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
+};
+
+//opg 5
+let i = 0;
+while (i < 6) {
+    console.log(i);
+    i++;
+};
+
+//opg 6
+let ii = 2;
+while (ii < 11) {
+    ii++;
+    if (ii % 2 == 0)
+        console.log(ii)
+};
---
+++ 5.js
@@ -0,0 +1,29 @@
+//opg 6
+let numberOne = 5;
+let numberTwo = numberOne++;
+
+console.log(numberOne);
+
+//opg 7
+let numberThree = 5;
+let numberFour = numberThree--;
+
+console.log(numberThree);
+
+//opg 8
+let equalOne = 10;
+let equalTwo = 10;
+
+let equals = equalOne == equalTwo;
+console.log(equals);
+
+//opg 9
+let xyz = 8;
+let abc = 2;
+
+let xyabdf = xyz > 5 && abc < 10;
+console.log(xyabdf);
+
+//opg 10
+let ternary = xyz > 10 ? 'Greater than' : 'Less than';
+console.log(ternary);
